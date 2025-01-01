e04846b3979aaf91094ce7b5ef13984628bc3f2a
StarryPurple <starrypurple@163.com>
2025-01-01 18:58:07 +0800

    dev note: Wrote a blocklist multimap. Give a try to multimap test.

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@
 void multimap_test() {
   using KeyType = StarryPurple::ConstStr<64>;
   using ValueType = int;
-  Insomnia::BlocklistMultimap<KeyType, ValueType, 40, StarryPurple::cCapacity> fmultimap;
+  Insomnia::BlocklistMultimap<KeyType, ValueType, 400, 32> fmultimap;
   fmultimap.open("tst");
   int n; std::cin >> n;
   std::string op, key_str;
